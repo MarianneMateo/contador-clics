b3a933507aa037d69db8641202a5305110b802c8
MarianneMateo <mariannejmb18@gmail.com>
2022-05-10 12:05:55 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+  const aumentar = () => {
+    setCount(count => count + 1);
+  }
+
+  const reiniciar = () => {
+    setCount(0);
+  }
+
+	return (
+    <div className='container'>
+      <div className='contador'>
+          {count}
+      </div>
+      <button onClick={aumentar}>Clic</button>
+      <button onClick={reiniciar}>Reiniciar</button>
+    </div>
+	);
+}
+
+export default App;
